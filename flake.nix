346f540a86c57b8b9fddb9e5719a1a1501360a55
misterclayt0n <daviarantes13@gmail.com>
2025-11-22 19:00:59 -0300

    nix: add flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Vanda studio's official flake";
+
+  inputs = {
+    nixpkgs.url     = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            typescript-language-server
+          ];
+        };
+      }
+    );
+}
