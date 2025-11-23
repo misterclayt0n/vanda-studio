60cc4efafe8d3fa0879159aca9191e5d2d175238
misterclayt0n <daviarantes13@gmail.com>
2025-11-23 20:28:05 -0300

    convex: remove unused import

--- app/ConvexClientProvider.tsx
+++ app/ConvexClientProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { ConvexReactClient } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ReactNode } from "react";
 import { useAuth } from '@clerk/nextjs';
