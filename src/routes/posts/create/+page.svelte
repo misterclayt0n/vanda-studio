6ddddd3073c836d44fd34877188b287ce1a5ec3b
misterclayt0n <daviarantes13@gmail.com>
2026-01-14 15:48:51 -0300

    fix typo

--- src/routes/posts/create/+page.svelte
+++ src/routes/posts/create/+page.svelte
@@ -315,7 +315,7 @@ Qual é o seu ritual matinal inegociável?
 							<svg class="h-4 w-4 text-muted-foreground" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
 								<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15" />
 							</svg>
-							<Label class="text-sm font-medium">Proporcao</Label>
+							<Label class="text-sm font-medium">Proporção</Label>
 						</div>
 						<AspectRatioSelector 
 							value={aspectRatio}
@@ -331,7 +331,7 @@ Qual é o seu ritual matinal inegociável?
 							<svg class="h-4 w-4 text-muted-foreground" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
 								<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z" />
 							</svg>
-							<Label class="text-sm font-medium">Resolucao</Label>
+							<Label class="text-sm font-medium">Resolução</Label>
 						</div>
 						<ResolutionSelector 
 							value={resolution}
